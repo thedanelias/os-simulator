2fb263904b29de901f4c119d2aff1dc46e5a6009
Dan Elias <dan.elias@outlook.com>
2025-07-25 16:57:16 +1000

    add skeleton of functions

--- linked_list.c
+++ linked_list.c
@@ -18,3 +18,11 @@ void destroyNode(nodeT* node){
   assert(node);
   free(node);
 }
+
+LinkedListT* create_LinkedList() {}
+void destroy_LinkedList(LinkedListT* list) {}
+void prepend_LinkedList(LinkedListT* list, int data) {}
+void append_LinkedList(LinkedListT* list, int data) {}
+int pop_front_LinkedList(LinkedListT* list) {}
+int pop_back_LinkedList(LinkedListT* list) {}
+int get_length_LinkedList(LinkedListT* list) {}
